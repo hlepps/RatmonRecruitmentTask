badc5a4ae5380b788f058d321e91bf600f98e1f1
hlepps <pomapiotrek@gmail.com>
2025-09-02 13:09:50 +0200

    feat: server app initial commit

---
+++ RatmonRecruitmentTaskSolution/Server/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Server.Components.Layout
+{
+    public partial class MainLayout : LayoutComponentBase, IDisposable
+    {
+        bool sidebarExpanded = true;
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ RatmonRecruitmentTaskSolution/Server/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Server.Components.Layout
+{
+    public partial class NavMenu : ComponentBase
+    {
+    }
+}
